2b1ce5a7484d15a765ccf39f318309bd5d17cb2f
Lara Zebiane <150203195+larazebiane@users.noreply.github.com>
2026-04-24 21:10:48 -0400

    Update constraintData.m

--- src/constraintData.m
+++ src/constraintData.m
@@ -2,7 +2,7 @@ function X_constraints = constraintData(X_train,n_constraints)
 
 % Description : Sets constraint data for trainNeuralNetwork
 % Authors     : Lara Zebiane and Frank E. Curtis
-% Inputs      : X_train, complete set of training inputs 
+% Inputs      : X_train, complete set of training inputs
 %               n_constraints, number of desired constraints
 % Outputs     : X_constraints, constraint inputs
 
